0d32c095e62cd1ae5b1ae913f90513aafe466f7e
Amarjit Singh <amarjitmca04@gmail.com>
2021-06-17 10:11:56 +0530

    Changes Committed: 	Added Summation of Four Int Arugments 	Refactored Java Code

--- CCode/addtion.c
+++ CCode/addtion.c
@@ -8,3 +8,8 @@ int sun(int x, int y) {
 int sum3(int x, int y, int z) {
 	return x + y + z
 }
+
+int sum4(int x, int y, int z, int t) {
+	return x + y + z + t
+}
+
--- JavaCode/Hello.java
+++ JavaCode/Hello.java
@@ -3,6 +3,10 @@ package learnJava;
 
 public class Hello {
     public static void main(String[] args) {
+    	helloWorld();
+    }
+
+    public void helloWord() {
         System.out.println("Hello, World!");
     }
 }
