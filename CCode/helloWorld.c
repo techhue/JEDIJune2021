e93d0279ef6f7bde96755381806c362465761270
techhue <amarjitmca04@gmail.com>
2021-06-17 11:36:48 +0530

    Update helloWorld.c

--- CCode/helloWorld.c
+++ CCode/helloWorld.c
@@ -3,12 +3,17 @@
 
 #include <stdio.h>
 
+void greeting();
 void helloWorld();
 
 int main() {
 	helloWorld();
 }
 
+void greeting() {
+	printf("Good Mornign!!!")
+}
+	       
 void helloWorld() {
 	printf("Hello World!");
 	printf("Awseome World!!!");
